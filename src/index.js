cf78507743cd8fd82d516ef7b676c7e016ee27d5
Yuusuf Mohamed <yuusufmoha440@gmail.com>
2026-09-08 16:49:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 export default {
   async fetch(request, env) {
-    return new Response("Haleel Real Estate Worker is working!", {
-      headers: {
-        "content-type": "text/plain;charset=UTF-8"
-      }
-    });
+    return env.ASSETS.fetch(request);
   }
 };
