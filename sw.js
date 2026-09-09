cf885a285ba8a028a56710e921a1164101d58c3f
Yuusuf Mohamed <yuusufmoha440@gmail.com>
2026-09-10 00:49:45 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "haleel-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
